5b0e53e8489a911fc4478a1f9c149fcc04a3568a
pujan <pujantamang92@gmail.com>
2021-09-01 19:02:21 +0545

    before latest_native_main relation

--- news_sarathi/en_app/admin.py
+++ news_sarathi/en_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import en_news, en_sports, en_latest_news, en_article, en_interview, en_editorial, en_main_news
 
-# Register your models here.
+
+admin.site.register(en_news)
+admin.site.register(en_sports)
+admin.site.register(en_latest_news)
+admin.site.register(en_article)
+admin.site.register(en_interview)
+admin.site.register(en_editorial)
+admin.site.register(en_main_news)
